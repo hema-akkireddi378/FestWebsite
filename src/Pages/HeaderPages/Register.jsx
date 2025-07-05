10811e60e5e227d601f9064170b33eb6c3ad2e90
Akkireddi Hema <akkireddihema@gmail.com>
2025-07-05 14:58:50 +0530

    register page updated

--- src/Pages/HeaderPages/Register.jsx
+++ src/Pages/HeaderPages/Register.jsx
@@ -1,38 +1,83 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 export default function Register() {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState(
+        {
+            username: '',
+            email: '',
+            password: '',
+            confirmpassword: '',
+
+        }
+    );
+    const handleChange = e => {
+        setFormData(prev =>
+        ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    };
+    const handleSubmit = e => {
+        e.preventDefault();
+        fetch('http://localhost:8081/api/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setFormData({ username: '', email: '', password: '', confirmpassword: '' });
+            })
+            .then(data => {
+                navigate("/Home");
+            });
+
+    };
     return (
         <div className="main-register">
             <div className="registe">
-                {/* <p className='register'>Register</p> */}
-                <div className="register-elements">
-                    <input type="text" placeholder="UserName" name="username" id="username" className="login-details">
-                    </input>
-                    <input type="password" placeholder="Password" name="password" id="password" className="login-details">
-                    </input>
-                    <input type="password" placeholder="Confirm password" name="confirm password" id="confirm password" className="login-details">
-                    </input>
-                    <input type="email" placeholder="E-mail address" name="email" id="email" className="login-details">
-                    </input>
-                    <div className="register-details-verify">
-                        <input type="checkbox" ></input>
-                        <label><span>Verify you are human</span></label>
-                    </div>
-                    <Link to="/Home" className="login-link">
-                    <button type="submit" className="login-button"><span>Sign Up</span></button>
-                    </Link>
-                </div>
-                <div className="signin">
-                    <p className="sign-in">Have an account
-                        <Link to="/SignIn" className="sign-in-link">Sign in</Link>
-                    </p>
-                    <p className="other">or you can sign in with,</p>
-                </div>
-                <div className="social-media">
-                   <img src="https://img.icons8.com/?size=100&id=118466&format=png&color=1A1A1A" alt="Facebook logo" className="facebook-link" height="30px" width="40%"></img>
-                    <img src="https://img.icons8.com/?size=100&id=17935&format=png&color=1A1A1A" alt="Google logo" className="google-link" height="30px" width="40px"></img>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="register-elements">
 
+                        <input type="text" placeholder="UserName" name="username" id="username" className="login-details" value={formData.username}
+                            onChange={handleChange}></input>
+                        <input type="email" placeholder="E-mail address" name="email" id="email" className="login-details" value={formData.email}
+                            onChange={handleChange}></input>
+                        <input type="password" placeholder="Password" name="password" id="password" className="login-details" value={formData.password}
+                            onChange={handleChange}></input>
+                        <input type="password" placeholder="confirm password" name="confirmpassword" id="confirmpassword" className="login-details" value={formData.confirmpassword}
+                            onChange={handleChange}></input>
+                        
+<div className="register-details-verify">
+    <input type="checkbox" required />
+    <label htmlFor="verify"><span>Verify you are human</span></label>
+</div>
+
+
+
+                        {/* <div className="register-details-verify">
+                            <input type="checkbox" ></input>
+                            <label><span>Verify you are human</span></label>
+                        </div> */}
+                        <div className="login-link">
+                            <button type="submit" className="login-button"><span>Sign Up</span></button>
+                        </div>
+                    </div>
+                    <div className="signin">
+                        <p className="sign-in">Have an account
+                            <Link to="/SignIn" className="sign-in-link">Sign in</Link>
+                        </p>
+                        <p className="other">or you can sign in with,</p>
+                    </div>
+                    <div className="social-media">
+                        <img src="https://img.icons8.com/?size=100&id=118466&format=png&color=1A1A1A" alt="Facebook logo" className="facebook-link" height="30px" width="40%"></img>
+                        <img src="https://img.icons8.com/?size=100&id=17935&format=png&color=1A1A1A" alt="Google logo" className="google-link" height="30px" width="40px"></img>
+                    </div>
+                </form>
             </div>
+
         </div>
     )
 }
